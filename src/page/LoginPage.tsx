52a0ba608ebed2e922081a120f33e06541ecd9c1
first.yuta <634230025@webmail.npru.ac.th>
2023-05-28 11:46:41 +0700

    update moo css

--- src/page/LoginPage.tsx
+++ src/page/LoginPage.tsx
@@ -5,7 +5,7 @@ const LoginPage:FC = () => {
         <>
     <div className = "flex w-screen h-screen bg-red-400">
         <div className="flex flex-col border rounded-lg p-4 shadow-md border-blue-400 bg-white w-1/2 m-auto w-full sm:w-1/2 md:w-1/3">
-            <img className='w-64 h-64 m-auto mt-6' src="moo.jpg" />
+            <img className='w-64 h-64 m-auto mt-3 mb-3 rounded-md' src="moo.jpg" />
             <h1 className='m-auto text-3xl' >Login</h1>
             <label htmlFor='email'>Email</label>
             <input id="email" type ="text"  className="npru-input" />
