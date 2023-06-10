7555b339dcdc16e0566e0329198e9abed1d0726f
first.yuta <634230025@webmail.npru.ac.th>
2023-06-10 14:55:59 +0700

    no comment

--- src/page/NewsPage.tsx
+++ src/page/NewsPage.tsx
@@ -1,13 +1,42 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+
+interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
+interface NewsProps {
+    data: Post;
+}
+
+const NewsComponent: FC<NewsProps> = ( { data } ) => {
+    return( 
+        <>
+        <div>{data.title}</div>
+        <div>{data.body}</div>
+        </>
+    );
+};
 
 const NewsPage: FC = () => {
+    //state
+        const [posts, setPosts] = useState<Post[]>([ {
+            userId: 1,
+            id: 1,
+            title: "test",
+            body: "test",
+            }]);
+       
+
     // function
     const loadNews = () => {
 
         fetch("https://jsonplaceholder.typicode.com/posts")
         .then((resp)=> resp.json())
         .then((json) => {
-            console.log(json);
+            setPosts(json);
         })
     };
 
@@ -18,7 +47,10 @@ const NewsPage: FC = () => {
         <button className='npru-button' onClick={loadNews}>
             Load News
         </button>
-    </>
+        { posts.map((value) => (
+            <NewsComponent data ={value} />
+        ))}
+            </>
     );
 };
 
