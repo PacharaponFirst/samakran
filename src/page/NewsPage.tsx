b9bf3a528dc321de9f17a7a4f3636c58b12430fc
first.yuta <634230025@webmail.npru.ac.th>
2023-06-10 14:11:30 +0700

    json

--- src/page/NewsPage.tsx
+++ src/page/NewsPage.tsx
@@ -1,11 +1,25 @@
 import { FC } from 'react';
 
 const NewsPage: FC = () => {
+    // function
+    const loadNews = () => {
+
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((resp)=> resp.json())
+        .then((json) => {
+            console.log(json);
+        })
+    };
+
+    //template
     return (
     <>
         <h1>NEWS TODAY!!!</h1>
+        <button className='npru-button' onClick={loadNews}>
+            Load News
+        </button>
     </>
-    )
+    );
 };
 
 export {NewsPage};
