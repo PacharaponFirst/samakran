4e1adca2db9f332c610045e76db371d946a41644
first.yuta <634230025@webmail.npru.ac.th>
2023-06-10 11:10:00 +0700

    responsive

--- src/page/MenuPage.tsx
+++ src/page/MenuPage.tsx
@@ -3,7 +3,14 @@ import { FC } from 'react';
 const MenuPage: FC = () => {
     return(
     <>
-    <h1>Menu</h1>
+        <div className='grid grid-cols-1 gap-6 p-6 md:grid-cols-2 lg:grid-cols-3'>
+            <button className='npru-button'>Menu #1</button>
+            <button className='npru-button'>Menu #2</button>
+            <button className='npru-button'>Menu #3</button>
+            <button className='npru-button'>Menu #4</button>
+            <button className='npru-button'>Menu #5</button>
+            <button className='npru-button'>LOGOUT</button>
+        </div>
     </>
     );
 };
