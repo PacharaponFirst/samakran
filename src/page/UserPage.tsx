7cceaa60267fd90bdc3162c387d6a0b53a911b8b
first.yuta <634230025@webmail.npru.ac.th>
2023-06-10 16:20:06 +0700

    table

--- src/page/UserPage.tsx
+++ src/page/UserPage.tsx
@@ -4,6 +4,24 @@ const UserPage: FC = () => {
     return(
     <>
     <h1>User Sawatdeekub</h1>
+    <table>
+        <thead>
+            <tr>
+                <th rowSpan={2}>Id</th>
+                <th rowSpan={2}>Name</th>
+                <th rowSpan={2}>User Name</th>
+                <th rowSpan={2}>Email</th>
+                <th colSpan={5}>Address</th>
+            </tr>
+            <tr>
+                <th>Street</th>
+                <th>Suite</th>
+                <th>City</th>
+                <th>Zipcode</th>
+                <th>Geo</th>
+            </tr>
+        </thead>
+    </table>
     </>
     );
 };
