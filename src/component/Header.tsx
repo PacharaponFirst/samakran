c8078d5eefac589553215130f1f1a9f63c6f13e1
first.yuta <634230025@webmail.npru.ac.th>
2023-06-10 10:42:36 +0700

    Header Link

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom';
 const Header: FC = () => {
     return(
     <>
-    <h1>LOGO</h1>
-    <Link to = "/admin/home">Home</Link>
-    <Link to = "/admin/menu">Menu</Link>
-    </>
+        <div className='flex flex-row gap-4 p-4'>
+             <h1>LOGO</h1>
+            <Link to = "/admin/home" className='bg-blue-500 text-gray-500 p-2 '>
+                Home</Link>
+            <Link to = "/admin/menu">Menu</Link>
+        </div>
+        </>
     );
 };
 
