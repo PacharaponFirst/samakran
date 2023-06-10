79707529e5bdef7b2a353ac664218e6b4d8dc96a
first.yuta <634230025@webmail.npru.ac.th>
2023-06-10 16:28:10 +0700

    table info

--- src/page/UserPage.tsx
+++ src/page/UserPage.tsx
@@ -12,6 +12,9 @@ const UserPage: FC = () => {
                 <th rowSpan={2}>User Name</th>
                 <th rowSpan={2}>Email</th>
                 <th colSpan={5}>Address</th>
+                <th rowSpan={2}>Phone</th>
+                <th rowSpan={2}>Website</th>
+                <th colSpan={3}>Company</th>
             </tr>
             <tr>
                 <th>Street</th>
@@ -19,8 +22,29 @@ const UserPage: FC = () => {
                 <th>City</th>
                 <th>Zipcode</th>
                 <th>Geo</th>
+                <th>Name</th>
+                <th>Catchpharse</th>
+                <th>Bs</th>
             </tr>
         </thead>
+        <tbody>
+            <tr>
+                <td>001</td>
+                <td>Jame</td>
+                <td>a@g.com</td>
+                <td>153/2</td>
+                <td>024-981-255</td>
+                <td>1.com</td>
+                <td>123/456</td>
+                <td>xxxx</td>
+                <td>BKK</td>
+                <td>123456</td>
+                <td>80, 100</td>
+                <td>Google</td>
+                <td>GH</td>
+                <td>189000</td>
+            </tr>
+        </tbody>
     </table>
     </>
     );
